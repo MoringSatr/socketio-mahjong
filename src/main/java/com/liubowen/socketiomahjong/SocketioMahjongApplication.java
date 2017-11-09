5bbc039d6d66b3291ccd126de27770a3db04a3a7
liubowen <liubowen_94@163.com>
2017-11-09 13:01:03 +0800

    Initial commit

---
+++ src/main/java/com/liubowen/socketiomahjong/SocketioMahjongApplication.java
@@ -0,0 +1,12 @@
+package com.liubowen.socketiomahjong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketioMahjongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketioMahjongApplication.class, args);
+	}
+}
---
+++ src/main/java/com/liubowen/socketiomahjong/config/Swagger2Configuration.java
@@ -0,0 +1,34 @@
+package com.liubowen.socketiomahjong.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+
+    @Bean
+    public Docket buildDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInf())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.liubowen.socketiomahjong.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo buildApiInf() {
+        return new ApiInfoBuilder()
+                .title("security-login-check 接口文档")
+                .description("security-login-check 提供的接口描述文档")
+                .build();
+    }
+
+}
