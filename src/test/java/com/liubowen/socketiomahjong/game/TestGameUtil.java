375c09c27094b9b1a3303893408b32023c01d2eb
liubowen <liubowen_94@163.com>
2017-12-29 02:14:42 +0800

    update
    Signed-off-by: liubowen <liubowen_94@163.com>

--- src/test/java/com/liubowen/socketiomahjong/game/TestGameUtil.java
+++ src/test/java/com/liubowen/socketiomahjong/game/TestGameUtil.java
@@ -1,5 +1,10 @@
 package com.liubowen.socketiomahjong.game;
 
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author liubowen
  * @date 2017/12/29 1:49
@@ -22,4 +27,23 @@ public class TestGameUtil {
     /** 判断玩家是否可以吃牌 */
 
     /**  */
+
+    public static void main(String[] args) throws Exception {
+        List<Integer> integers = Lists.newArrayList();
+        integers.add(1);
+        integers.add(2);
+        integers.add(2);
+        integers.add(3);
+        integers.add(5);
+        integers.add(7);
+        integers.add(8);
+        integers.add(11);
+
+        System.err.println(Arrays.toString(integers.toArray()));
+        integers.remove((Integer) 11);
+        integers.remove((Integer) 2);
+        integers.remove((Integer) 2);
+        integers.remove((Integer) 5);
+        System.err.println(Arrays.toString(integers.toArray()));
+    }
 }
